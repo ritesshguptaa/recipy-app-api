8832552bdda16304b90d7471475d0adabf31f5f4
Er Ritesh Gupta <riteshgupta@Ers-MacBook-Pro.local>
2020-10-21 12:08:17 +0530

    updated admin to support custom user mode

--- app/core/models.py
+++ app/core/models.py
@@ -15,12 +15,10 @@ class UserManager(BaseUserManager):
         user = self.model(email=self.normalize_email(email),**extra_fields)
         user.set_password(password)
         user.save(using=self._db)
-
         return user
 
     def create_superuser(self,email,password):
         """Create and save superuser"""
-
         user = self.create_user(email,password)
         user.is_staff = True
         user.is_superuser = True
