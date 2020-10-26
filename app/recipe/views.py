a979777c864730415de6ca6398c14b51a67542f9
Er Ritesh Gupta <riteshgupta@Ers-MacBook-Pro.local>
2020-10-26 10:50:23 +0530

    ingredient end point created

--- app/recipe/views.py
+++ app/recipe/views.py
@@ -35,6 +35,6 @@ class IngredientViewSet(viewsets.GenericViewSet,
         """Return objects for current authenticate user only"""
         return self.queryset.filter(user=self.request.user).order_by('-name')
 
-    # def perform_create(self, serializer):
-    #     """Create a new tag"""
-    #     serializer.save(user=self.request.user)
+    def perform_create(self, serializer):
+        """Create a new Ingredient"""
+        serializer.save(user=self.request.user)
